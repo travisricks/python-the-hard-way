093bef4cff2adf0f18554ad619ac2bc72f80a8d1
travisricks <ricks.travis@gmail.com>
2015-08-02 16:11:03 -0400

    ex14-16

---
+++ ex15.py
@@ -0,0 +1,15 @@
+from sys import argv # get the argv feature from the sys package
+
+script, filename = argv
+
+txt = open(filename)
+
+print "Here's your file %r:" % filename
+print txt.read()
+
+print "Type the filename again:"
+file_again = raw_input("> ")
+
+txt_again = open(file_again)
+
+print txt_again.read()
